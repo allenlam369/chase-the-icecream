b1d44f5a70531939eddcf12ce248e66f02e3d3b5
allenlam369 <32602628+allenlam369@users.noreply.github.com>
2022-09-27 10:11:02 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,47 @@
+def on_on_overlap(sprite, otherSprite):
+    icecream.set_position(randint(10, 160), randint(10, 120))
+    info.change_score_by(1)
+    info.start_countdown(5)
+sprites.on_overlap(SpriteKind.player, SpriteKind.food, on_on_overlap)
+
+icecream: Sprite = None
+scene.set_background_color(13)
+mySprite = sprites.create(img("""
+        . . . . . . . . . . b 5 b . . .
+            . . . . . . . . . b 5 b . . . .
+            . . . . . . b b b b b b . . . .
+            . . . . . b b 5 5 5 5 5 b . . .
+            . . . . b b 5 d 1 f 5 d 4 c . .
+            . . . . b 5 5 1 f f d d 4 4 4 b
+            . . . . b 5 5 d f b 4 4 4 4 b .
+            . . . b d 5 5 5 5 4 4 4 4 b . .
+            . . b d d 5 5 5 5 5 5 5 5 b . .
+            . b d d d d 5 5 5 5 5 5 5 5 b .
+            b d d d b b b 5 5 5 5 5 5 5 b .
+            c d d b 5 5 d c 5 5 5 5 5 5 b .
+            c b b d 5 d c d 5 5 5 5 5 5 b .
+            . b 5 5 b c d d 5 5 5 5 5 d b .
+            b b c c c d d d d 5 5 5 b b . .
+            . . . c c c c c c c c b b . . .
+    """),
+    SpriteKind.player)
+icecream = sprites.create(img("""
+        . . . . . 3 3 b 3 3 d d 3 3 . . 
+            . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
+            . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
+            . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
+            . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
+            . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
+            . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
+            . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
+            . 4 4 d 1 1 1 1 1 1 d d d b b . 
+            . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+            4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+            4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+            4 5 5 d 5 5 d b b b d d 3 . . . 
+            4 5 5 5 d d d d 4 4 b 3 . . . . 
+            . 4 5 5 5 4 4 4 . . . . . . . . 
+            . . 4 4 4 . . . . . . . . . . .
+    """),
+    SpriteKind.food)
+controller.move_sprite(mySprite)
---
+++ main.ts
@@ -0,0 +1,44 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    icecream.setPosition(randint(10, 160), randint(10, 120))
+    info.changeScoreBy(1)
+    info.startCountdown(5)
+})
+let icecream: Sprite = null
+scene.setBackgroundColor(13)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . b 5 b . . .
+    . . . . . . . . . b 5 b . . . .
+    . . . . . . b b b b b b . . . .
+    . . . . . b b 5 5 5 5 5 b . . .
+    . . . . b b 5 d 1 f 5 d 4 c . .
+    . . . . b 5 5 1 f f d d 4 4 4 b
+    . . . . b 5 5 d f b 4 4 4 4 b .
+    . . . b d 5 5 5 5 4 4 4 4 b . .
+    . . b d d 5 5 5 5 5 5 5 5 b . .
+    . b d d d d 5 5 5 5 5 5 5 5 b .
+    b d d d b b b 5 5 5 5 5 5 5 b .
+    c d d b 5 5 d c 5 5 5 5 5 5 b .
+    c b b d 5 d c d 5 5 5 5 5 5 b .
+    . b 5 5 b c d d 5 5 5 5 5 d b .
+    b b c c c d d d d 5 5 5 b b . .
+    . . . c c c c c c c c b b . . .
+`, SpriteKind.Player)
+icecream = sprites.create(img`
+    . . . . . 3 3 b 3 3 d d 3 3 . . 
+    . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
+    . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
+    . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
+    . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
+    . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
+    . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
+    . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
+    . 4 4 d 1 1 1 1 1 1 d d d b b . 
+    . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+    4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+    4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+    4 5 5 d 5 5 d b b b d d 3 . . . 
+    4 5 5 5 d d d d 4 4 b 3 . . . . 
+    . 4 5 5 5 4 4 4 . . . . . . . . 
+    . . 4 4 4 . . . . . . . . . . . 
+    `, SpriteKind.Food)
+controller.moveSprite(mySprite)
